342ac553e867a8c9fa311a7c0ebba987c8cc3cdf
Maligireddy Dinesh Reddy <maligireddydineshreddy@gmail.com>
2026-08-09 19:03:33 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -5,22 +5,47 @@ client = Client()
 
 
 
+# ==========================
+# SUPPORTED CRYPTO
+# ==========================
+
+COMMON_CRYPTO = [
+
+    # Hot buttons
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT",
+    "XRPUSDT",
+
+
+    # Backend only
+    "BNBUSDT"
+
+]
+
+
+
+
+
+# ==========================
+# GET CRYPTO PRICE
+# ==========================
+
+
 def get_crypto_price(symbol):
 
 
-    supported = [
-
-        "BTCUSDT",
-        "ETHUSDT",
-        "SOLUSDT"
-
-    ]
+    symbol = symbol.upper()
 
 
-    if symbol not in supported:
+
+    if symbol not in COMMON_CRYPTO:
+
 
         raise Exception(
+
             "Crypto symbol not supported"
+
         )
 
 
@@ -35,10 +60,60 @@ def get_crypto_price(symbol):
 
     return {
 
+
         "symbol": symbol,
 
+
         "price": float(
+
             ticker["price"]
+
         )
 
     }
+
+
+
+
+
+
+# ==========================
+# VALIDATE CRYPTO
+# ==========================
+
+
+def validate_crypto(symbol):
+
+
+    symbol = symbol.upper()
+
+
+
+    # Fast local check
+
+    if symbol in COMMON_CRYPTO:
+
+        return True
+
+
+
+    # Binance fallback
+
+    try:
+
+
+        client.get_symbol_ticker(
+
+            symbol=symbol
+
+        )
+
+
+        return True
+
+
+
+    except:
+
+
+        return False
