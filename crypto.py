c48475a2cc65ef98686f845286e7ac5614ac122a
Maligireddy Dinesh Reddy <maligireddydineshreddy@gmail.com>
2026-08-09 18:03:22 +0530

    Update and rename binance.py to crypto.py

--- crypto.py
+++ crypto.py
@@ -6,18 +6,20 @@ client = Client()
 
 def get_crypto_price(symbol):
 
-    symbol_map = {
-        "BTCUSDT": "BTCUSDT",
-        "ETHUSDT": "ETHUSDT"
-    }
+    supported = [
+        "BTCUSDT",
+        "ETHUSDT"
+    ]
 
 
-    if symbol not in symbol_map:
-        raise Exception("Crypto symbol not supported")
+    if symbol not in supported:
+        raise Exception(
+            "Crypto symbol not supported"
+        )
 
 
     ticker = client.get_symbol_ticker(
-        symbol=symbol_map[symbol]
+        symbol=symbol
     )
 
 
