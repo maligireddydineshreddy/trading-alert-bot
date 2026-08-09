1aabceb9a46ed942fce046f4b3b274fc96695d26
Maligireddy Dinesh Reddy <maligireddydineshreddy@gmail.com>
2026-08-09 18:08:36 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -4,26 +4,41 @@ from binance.client import Client
 client = Client()
 
 
+
 def get_crypto_price(symbol):
 
+
     supported = [
+
         "BTCUSDT",
-        "ETHUSDT"
+        "ETHUSDT",
+        "SOLUSDT"
+
     ]
 
 
     if symbol not in supported:
+
         raise Exception(
             "Crypto symbol not supported"
         )
 
 
+
     ticker = client.get_symbol_ticker(
+
         symbol=symbol
+
     )
 
 
+
     return {
+
         "symbol": symbol,
-        "price": float(ticker["price"])
+
+        "price": float(
+            ticker["price"]
+        )
+
     }
