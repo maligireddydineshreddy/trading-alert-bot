2b16fa9c7defeeddbe73983801766fb7b24781cd
Maligireddy Dinesh Reddy <maligireddydineshreddy@gmail.com>
2026-08-09 18:07:42 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -12,8 +12,11 @@ from database import (
 telegram_bot = None
 
 
+
 def set_bot(bot):
+
     global telegram_bot
+
     telegram_bot = bot
 
 
@@ -23,69 +26,99 @@ async def send_alert(user_id, message):
     if telegram_bot:
 
         await telegram_bot.send_message(
+
             chat_id=user_id,
+
             text=message
+
         )
 
 
 
 def get_current_price(symbol):
 
+
     crypto_symbols = [
+
         "BTCUSDT",
-        "ETHUSDT"
+        "ETHUSDT",
+        "SOLUSDT"
+
     ]
 
 
     if symbol in crypto_symbols:
 
+
         data = get_crypto_price(symbol)
 
+
         return float(
+
             data["price"]
+
         )
 
 
     else:
 
+
         data = get_price(symbol)
 
+
         return float(
+
             data["bid"]
+
         )
 
 
 
+
 async def check_alerts():
+
 
     alerts = get_active_alerts()
 
 
     for alert in alerts:
 
+
         alert_id = alert[0]
+
         user_id = alert[1]
+
         symbol = alert[2]
+
         target = float(alert[3])
+
 
 
         try:
 
+
             current = get_current_price(symbol)
 
 
+
             print(
+
                 f"{symbol} | {current} | Target {target}",
+
                 flush=True
+
             )
+
 
 
             if current >= target:
 
 
+
                 await send_alert(
 
                     user_id,
+
 
                     f"""
 🚨 PRICE ALERT HIT
@@ -103,31 +136,44 @@ Current:
                 )
 
 
+
                 disable_alert(alert_id)
 
 
 
         except Exception as e:
 
+
             print(
+
                 "Monitor error:",
+
                 e,
+
                 flush=True
+
             )
+
 
 
 
 
 async def monitor_loop():
 
+
     print(
+
         "📡 Monitor running",
+
         flush=True
+
     )
 
 
     while True:
 
+
         await check_alerts()
+
 
         await asyncio.sleep(10)
