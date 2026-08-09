c6addf3c1d8efa053567bc153757ae1832fadc30
Maligireddy Dinesh Reddy <maligireddydineshreddy@gmail.com>
2026-08-09 16:27:30 +0530

    Update fxcm.py

--- fxcm.py
+++ fxcm.py
@@ -1,91 +1,57 @@
 import os
-import requests
+import time
 
+from forexconnect import ForexConnect, fxcorepy
 
-FXCM_ENDPOINT = "https://endpoints-demo.fxcm.com"
 
 FXCM_USERNAME = os.getenv("FXCM_USERNAME")
 FXCM_PASSWORD = os.getenv("FXCM_PASSWORD")
 
 
-def create_session():
-
-    s = requests.Session()
-
-    # get trading system
-    r = s.get(
-        f"{FXCM_ENDPOINT}/iam/trading-systems/{FXCM_USERNAME}",
-        headers={
-            "X-COOKIE-DOMAIN": "fxcm.com"
-        }
-    )
-
-    r.raise_for_status()
-
-    system = r.json()[0]
-
-
-    xsrf = s.cookies.get("XSRF-TOKEN")
-
-
-    # login
-    r = s.post(
-        f"{FXCM_ENDPOINT}/iam/authenticate",
-        json={
-            "loginId": FXCM_USERNAME,
-            "password": FXCM_PASSWORD,
-            "tradingSessionId": system["tradingSessionId"],
-            "tradingSessionSubId": system["tradingSessionSubId"],
-            "appName": "TelegramTradingAlertBot"
-        },
-        headers={
-            "X-COOKIE-DOMAIN":"fxcm.com",
-            "X-XSRF-TOKEN":xsrf
-        }
-    )
-
-    r.raise_for_status()
-
-    data=r.json()
-
-    s.headers.update({
-        "Authorization": "Bearer " + data["accessToken"],
-        "X-COOKIE-DOMAIN":"fxcm.com"
-    })
-
-
-    return s
-
-
-
 def get_price(symbol="EUR/USD"):
 
-    s=create_session()
+    fx = ForexConnect()
+
+    try:
+
+        fx.login(
+            FXCM_USERNAME,
+            FXCM_PASSWORD,
+            "https://www.fxcorporate.com/Hosts.jsp",
+            "Demo"
+        )
+
+        offers = fx.get_table(
+            fxcorepy.O2GTableType.OFFERS
+        )
 
 
-    url=f"{FXCM_ENDPOINT}/trading/get_model"
+        for row in offers:
+
+            if row.instrument == symbol:
+
+                price = {
+                    "bid": row.bid,
+                    "ask": row.ask
+                }
+
+                fx.logout()
+
+                return price
 
 
-    params={
-        "models":"Offer",
-        "symbols":symbol
-    }
+        fx.logout()
+
+        raise Exception(
+            f"{symbol} not found"
+        )
 
 
-    r=s.get(
-        url,
-        params=params
-    )
+    except Exception as e:
 
+        try:
+            fx.logout()
+        except:
+            pass
 
-    print("STATUS:",r.status_code)
-    print(r.text[:500])
-
-
-    r.raise_for_status()
-
-
-    data=r.json()
-
-
-    return data
+        raise e
