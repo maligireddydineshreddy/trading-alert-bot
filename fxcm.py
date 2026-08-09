9b95a9783e239e18578077829715c2eeb3559017
Maligireddy Dinesh Reddy <maligireddydineshreddy@gmail.com>
2026-08-09 18:43:39 +0530

    Update fxcm.py

--- fxcm.py
+++ fxcm.py
@@ -1,52 +1,137 @@
 import os
 
-from forexconnect import ForexConnect, fxcorepy
+os.environ["LD_LIBRARY_PATH"] = "/app/forexconnect/lib"
+
+
+from forexconnect import (
+    ForexConnect,
+    fxcorepy
+)
+
+
+
+def login_fxcm():
+
+    fx = ForexConnect()
+
+
+    fx.login(
+
+        os.getenv("FXCM_USERNAME"),
+
+        os.getenv("FXCM_PASSWORD"),
+
+        os.getenv("FXCM_URL"),
+
+        "Demo"
+
+    )
+
+
+    return fx
+
+
+
 
 
 def get_price(symbol):
 
-    symbol_map = {
-        "EURUSD": "EUR/USD",
-        "GBPUSD": "GBP/USD",
-        "USDJPY": "USD/JPY",
-        "GBPJPY": "GBP/JPY",
-        "XAUUSD": "XAU/USD"
-    }
 
-    target = symbol_map.get(symbol, symbol)
+    symbol = symbol.upper().replace("/", "")
 
-    fx = ForexConnect()
 
-    fx.login(
-        os.getenv("FXCM_USERNAME"),
-        os.getenv("FXCM_PASSWORD"),
-        os.getenv("FXCM_URL"),
-        "Demo"
-    )
+    fx = login_fxcm()
+
 
 
     offers = fx.get_table(
+
         fxcorepy.O2GTableType.OFFERS
+
     )
+
 
 
     for row in offers:
 
-        if row.instrument == target:
 
-            data = {
+        fx_symbol = row.instrument.replace("/", "")
+
+
+
+        if fx_symbol == symbol:
+
+
+            price = {
+
+
                 "symbol": row.instrument,
+
                 "bid": row.bid,
+
                 "ask": row.ask
+
             }
+
 
             fx.logout()
 
-            return data
+
+            return price
+
 
 
     fx.logout()
 
+
     raise Exception(
+
         f"{symbol} not found"
+
     )
+
+
+
+
+
+
+
+def validate_symbol(symbol):
+
+
+    symbol = symbol.upper().replace("/", "")
+
+
+    fx = login_fxcm()
+
+
+
+    offers = fx.get_table(
+
+        fxcorepy.O2GTableType.OFFERS
+
+    )
+
+
+
+    for row in offers:
+
+
+        fx_symbol = row.instrument.replace("/", "")
+
+
+
+        if fx_symbol == symbol:
+
+
+            fx.logout()
+
+
+            return True
+
+
+
+    fx.logout()
+
+
+    return False
